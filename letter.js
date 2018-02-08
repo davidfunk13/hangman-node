2d4ff08814f7c2bb18c5ab8a64492e059d7e57e8
Dave Funk <Dave@Daves-MacBook-Pro.local>
2018-02-07 20:39:00 -0800

    changing machines

--- letter.js
+++ letter.js
@@ -1,1 +1,26 @@
-function Letter()
+var userGuess = process.argv;
+var wordsDude = 'moist';
+var blanksArray = [];
+var splitWordArray = [];
+
+
+function createBlanks() {
+    var charactersDude = Array.from(wordsDude);
+    splitWordArray = charactersDude
+    console.log(charactersDude)
+    for (var i = 0; i < charactersDude.length; i++) {
+        blanksArray.push('_');
+    }
+    console.log(blanksArray);
+}
+
+function Letter(userGuess) {
+    this.stringValue = userGuess.toString();
+    letterGuessed: false
+    this.checkLetter = function (userGuess) {
+        if 
+    }
+
+
+}
+createBlanks();
