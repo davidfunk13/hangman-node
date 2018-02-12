8d8f319047a5aa593ec7964b9d38ef031c1acc0a
Dave <davefunk135@gmail.com>
2018-02-11 20:32:37 -0800

    conditionals aded into guess letter boolean

--- letter.js
+++ letter.js
@@ -9,9 +9,12 @@ var Letter = function (char) {
         return ('_');
     }
     this.setBoolean = function (char) {
-        console.log(`made it to boolean with ${char}`)
+        // console.log(`made it to boolean with ${char}`)
         if (char === this.stringValue){
             console.log('ayo')
+        }
+        if (char !== this.stringValue) {
+            console.log('no dice')
         }
         // if (!this.isVisible) {
         //     console.log('setboolean function hit')
