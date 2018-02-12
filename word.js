81ceddbde4394414adbd86f9312e8197278d69c3
Dave <davefunk135@gmail.com>
2018-02-11 18:11:53 -0800

    returning characters using boolean function

--- word.js
+++ word.js
@@ -4,18 +4,16 @@ var Word = function (chosenWord) {
     this.word = chosenWord;
     this.charArray = Array.from(this.word);
     this.letterObjectsArray = [];
-    // this.check = function (char) {
-    //     for (var i = 0; i < this.letterObjectsArray.length; i++) {
-    //         console.log(letterObjectsArray[i])
-    //     }
-    // }
     this.isVisibleCheck = function (char) {
+        var charReturns = [];
         for (var i = 0; i < this.charArray.length; i++) {
             // console.log(this.charArray[i])
-            newLetter = new Letter(this.charArray[i])
+            var newLetter = new Letter(this.charArray[i])
             // console.log(newLetter)
             this.letterObjectsArray.push(newLetter);
+            charReturns.push(newLetter.revealCharacter());
         }
+        console.log(charReturns.join(" "))
     }
 }
 
